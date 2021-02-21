eb0f227f938892507e40ca3a43abf638024d07df
shiv123736 <60289301+shiv123736@users.noreply.github.com>
2021-02-21 16:02:02 +0530

    Update firebase.js

--- src/component/firebase.js
+++ src/component/firebase.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBkMEbLoBF6qreDwMDBvPc1HLHe3s8h5mk",
-    authDomain: "people-db-dd1ac.firebaseapp.com",
-    projectId: "people-db-dd1ac",
-    storageBucket: "people-db-dd1ac.appspot.com",
-    messagingSenderId: "354867158737",
-    appId: "1:354867158737:web:d97b00e98865231c15eff0"
-  };
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID",
+};
   // Initialize Firebase
  firebase.initializeApp(firebaseConfig);
 
